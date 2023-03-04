1ad86532411fcc5b18d44bd76d0b5e8f6b9604cd
Chris Fryer <99569921+Frynoceros@users.noreply.github.com>
2023-03-03 14:03:37 -1000

    Rails7/Vite

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite';
+import FullReload from 'vite-plugin-full-reload';
+import RubyPlugin from 'vite-plugin-ruby';
+
+export default defineConfig({
+  clearScreen: false,
+  plugins: [
+    RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'], {delay: 300}),
+  ],
+});
